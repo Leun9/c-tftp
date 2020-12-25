8bfadb123c700d4af495bcfab0cdf655f62a1b71
Leun9 <43788336+Leun9@users.noreply.github.com>
2020-12-26 03:20:35 +0800

    netascii

--- netascii.c
+++ netascii.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-// RFC 854
+// RFC 5198
 
 int CheckNetascii(const char *filename) {
     FILE* fp = fopen(filename, "rb");
@@ -12,11 +12,12 @@ int CheckNetascii(const char *filename) {
     for (; ; ) {
         lastch = ch;
         ch = fgetc(fp);
-        if (lastch == 0x0d && (ch != 0x0a && ch !=0x00)) ret = INVALID_CRorLF; // invalid CR
-        if (ch == 0x0a && lastch != 0x0d) ret = INVALID_CRorLF; // invalid LF
-        if (ch >= 0x20 && ch <= 0x7e) continue;
-        if (ch >= 0x07 && ch <= 0x0d) continue;
-        if (ch == 0) continue;
+        if (lastch == 0x0d && (ch != 0x0a && ch !=0x00)) /* 不合法的CR */
+            ret = INVALID_CRorLF;
+        if (ch == 0x0a && lastch != 0x0d) ret = INVALID_CRorLF; /* 不合法的LF */
+        if (ch >= 0x20 && ch <= 0x7e) continue; /* 0x20到0x7e之间的可打印字符 */
+        if (ch >= 0x07 && ch <= 0x0d) continue; /* 0x07到0x0d之的七个控制字符 */
+        if (ch == 0) continue;                  /* 控制字符NUL(0x00) */
         if (ch == EOF) break;
         fclose(fp);
         return INVALIDCHAR; // invalid char
@@ -39,7 +40,7 @@ int Txt2Netascii(const char *filename, const char *tmpfile) {
     for (; ; ) {
         lastch = ch;
         ch = fgetc(fpr);
-        if (lastch == 0x0d && (ch != 0x0a && ch !=0x00)) fputc(0x00, fpw); // invalid CR
+        if (lastch == 0x0d && (ch != 0x0a && ch !=0x00))fputc(0x00, fpw); // invalid CR
         if (ch == 0x0a && lastch != 0x0d) fputc(0x0d, fpw); // invalid LF
         if (ch == EOF) break;
         fputc(ch, fpw); 
